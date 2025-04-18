04c354b1c36b78ab26174bc03bdb2fe64b5dd4ad
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-04-18 20:34:05 +0200

    install and set settings for react router dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from "./App.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+
+// Define routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // Use App as the global layout for all pages
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+      <RouterProvider router={router} />
+
 )
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState } from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        Login Page
+    </div>
+  )
+}
+
+export default LoginPage
