68a963744cce6959f8048dff442c972408ce05e0
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-04-18 20:44:05 +0200

    add redirect from home page to login page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default HomePage
+  const token = false;
+  
+  useEffect(() => {
+    if (!token) {
+        navigate('/login')
+    }
+  }, [token])
+
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
