f1d1d3469e564984f588be89de1d479a8bd26e6a
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-05-03 21:51:58 +0200

    instal flask and create app.py

---
+++ ml/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from ml_logic import process_data
+
+app = Flask(__name__)
+
+# Простой POST-эндпоинт
+@app.route("/api/process", methods=["POST"])
+def process():
+    try:
+        data = request.get_json()
+        result = process_data(data)  # Call main function
+        return jsonify({"status": "ok", "result": result})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
