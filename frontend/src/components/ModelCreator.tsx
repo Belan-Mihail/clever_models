d12412b1e21e90cbf2c49d772daea4d5f81fcb9f
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-05-03 22:57:22 +0200

    edit creatorModel

--- frontend/src/components/ModelCreator.tsx
+++ frontend/src/components/ModelCreator.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const ModelCreator = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const [file, setFile] = useState<File | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [response, setResponse] = useState<string | null>(null);
+
+
+  const handleFileClick = () => {
+    fileInputRef.current?.click();
+  }
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0] || null;
@@ -36,18 +42,32 @@ const ModelCreator = () => {
 
   return (
     <div className="w-[85%] h-full p-4 bg-white">
-      <div className="max-w-max max-h-max p-4 border-2 border-amber-500 rounded-2xl">
+      <div className="max-w-max max-h-max p-8 border-2 border-gray-300  rounded-2xl flex flex-col gap-4">
+        {/* hidden input */}
         <input
           type="file"
           // accept='.xlsx'
           onChange={handleFileChange}
-          className="block bg-rose-500 rounded text-sm p-1"
+          className="bg-rose-500 rounded text-sm p-1 hidden"
+          ref={fileInputRef}
         />
+
+        {/* custom input */}
+        <button
+          onClick={handleFileClick}
+          // accept='.xlsx'
+          
+          className="bg-rose-500 hover:bg-rose-600 rounded text-xs p-2 w-30 text-white"
+          
+        >
+            {file ? `${file.name}` : "Add file"}
+        </button>
+
 
         {file && (
           <button
             onClick={handleUpload}
-            className="px-4 py-2 bg-amber-500 text-white hover:bg-white hover:text-amber-500 rounded"
+            className="text-xs p-2 bg-amber-500 text-white hover:bg-white hover:text-amber-500 hover:border-1 hover:border-amber-500 rounded"
           >
             {loading ? "Loading..." : "Start Processing"}
           </button>
