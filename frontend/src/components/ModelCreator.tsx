d0b5b79ce29b46081d88cad26a2120b1ea5cb4a2
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-05-07 21:55:26 +0200

    edit ModelCreator add firstAnalyse state

--- frontend/src/components/ModelCreator.tsx
+++ frontend/src/components/ModelCreator.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
 import FileUploader from "./StepByStepComponents/fileUploader";
+import SelectFeatures from "./StepByStepComponents/SelectFeatures";
 
 
 const ModelCreator = () => {
-  
+  const [firstStepGeneralAnalyse, setFirstStepGeneralAnalyse] = useState<any>(null);
+
+  const handleFirstStepGeneralAnalyse = (data:any) => {
+    setFirstStepGeneralAnalyse(data)
+  }
 
   return (
     <div className="w-[85%] h-full p-4 bg-white">
-      <FileUploader />
+      <FileUploader handleFirstStepGeneralAnalyse={handleFirstStepGeneralAnalyse} />
+      {firstStepGeneralAnalyse && <SelectFeatures />}
     </div>
   );
 };
