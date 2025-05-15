f977224fa20d1ecbe8f5e55d12177701d774b0a3
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-05-15 21:55:56 +0200

    create select_columns function in app.py

--- ml/app.py
+++ ml/app.py
@@ -26,10 +26,31 @@ def process():
         # main logic
         result = first_process_data(file)  
 
-        return jsonify({"status": "ok", "result": result})
+        return jsonify({"status": "ok", **result})
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+
+
+@app.route("api/select_columns/", methods=["POST"])
+def select_columns():
+    try:
+        data = request.json
+        session_id = data.get("session_id")
+        x_col = data.get("x")
+        y_col = data.get("y")
+    
+        from ml_logic import load_saved_df
+        df = load_saved_df(session_id)
+
+        if x_col not in df.columns or x_col not in df.columns:
+            return jsonify({"status": "error", "message": "Invalid columns"}), 400
+    
+        return jsonify({"status": "ok"})
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
